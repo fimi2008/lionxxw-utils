6ce967a0828a933ff9ecb8274a52a3a3d6a77af7
wangjian <wangjian@baofoo.com>
2016-11-08 16:05:55 +0800

    提交测试代码

--- src/test/java/test/TestSubChar.java
+++ src/test/java/test/TestSubChar.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
 
 /**
  * 截取字符串
+ * 在控制台输入一个字符串，以及需要截取的字节数，则根据要求输出对应的字符串。【注意:保证汉字不被截半个】
+ * 例如:
+ * 我ABCD + 5 ==> 我ABC
+ * 我AB测试BCD + 8 ==> 我AB测试
+ * 我AB测试BDD + 7 ==> 我AB测
+ *
  * Created on 2016/11/7 17:40
  * version 1.0.0
  */
@@ -24,13 +30,33 @@ public class TestSubChar {
             return "截取起点为负!";
         }
 
-        byte[] b = str.getBytes();
-        if (len != 0) {
-            if (b[len - 1] < 0) {
-                if (b[len] < 0)
-                    len = len - 1;
+        char[] chars = str.toCharArray();
+        for (int i = 0,size = chars.length; i < size; i++){
+            if (isChinese(chars[i])){
+                len--;
+                if (len <= 0){
+                    len = i; // 保证汉字不被截半个
+                }
             }
         }
-        return new String(b, 0, len);
+        return str.substring(0, len);
+    }
+
+    /**
+     * 是否为中文
+     * @param c
+     * @return
+     */
+    private static final boolean isChinese(char c) {
+        Character.UnicodeBlock ub = Character.UnicodeBlock.of(c);
+        if (ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS
+                || ub == Character.UnicodeBlock.CJK_COMPATIBILITY_IDEOGRAPHS
+                || ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_A
+                || ub == Character.UnicodeBlock.GENERAL_PUNCTUATION
+                || ub == Character.UnicodeBlock.CJK_SYMBOLS_AND_PUNCTUATION
+                || ub == Character.UnicodeBlock.HALFWIDTH_AND_FULLWIDTH_FORMS) {
+            return true;
+        }
+        return false;
     }
 }
